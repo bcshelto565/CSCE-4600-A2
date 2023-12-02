f89d9d78793f330354bbbea2e2a5b5680df2956b
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 17:29:53 -0600

    Update echo.go

--- builtins/echo.go
+++ builtins/echo.go
@@ -16,6 +16,6 @@ func Echo(w io.Writer, args ...string) (string, error) {
 		return fmt.Errorf("%w: expected at least one argument, echo is used to echo a string, need a string to echo", ErrInvalidArgCountEcho)
 	default:
 		ech := fmt.Sprint(args)
-		return (ech)
+		return (ech, nil)
 	}
 }
