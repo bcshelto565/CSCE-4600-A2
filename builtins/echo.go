5af506f9a7ec934aed3574a556450c1f0a595324
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 19:31:47 -0600

    Update echo.go

--- builtins/echo.go
+++ builtins/echo.go
@@ -8,6 +8,7 @@ import (
 
 var (
 	ErrInvalidArgCountEcho = errors.New("invalid argument count")
+	PlaceholderErr = errors.New("")
 )
 
 func Echo(w io.Writer, args ...string) (error) {
@@ -16,6 +17,7 @@ func Echo(w io.Writer, args ...string) (error) {
 		return fmt.Errorf("%w: expected at least one argument, echo is used to echo a string, need a string to echo", ErrInvalidArgCountEcho)
 	default:
 		ech := fmt.Sprint(args)
-		return fmt.Errorf("%w",NonError,ech)
+		PlaceholderErr = errors.New(ech)
+		return fmt.Errorf("%w",PlaceholderErr)
 	}
 }
