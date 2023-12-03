154058cd47fde5d7c8c2496d80be82ef655c1ba2
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 19:40:51 -0600

    Update alias.go

--- builtins/alias.go
+++ builtins/alias.go
@@ -25,24 +25,24 @@ func printComs(aliasSlic []ComAlias) {		// simple for loop function to print ent
 	return
 }
 
-func CommandAlias(w io.Writer, args ...string) error {
+func CommandAlias(w io.Writer, args ...string) (string, error) {
 	switch len(args) {		// switch case for determining how to run the command
 	case 0:
-		return fmt.Errorf("%w: expected at least one argument, one for print or write of a name, and one for value of alias", ErrInvalidArgCountAlias)
+		return "", fmt.Errorf("%w: expected at least one argument, one for print or write of a name, and one for value of alias", ErrInvalidArgCountAlias)
 	case 1:
 		if args[0] == "-p" {
-			printComs(aliasSlic)
-			return (nil)
+			output := string(printComs(aliasSlic))
+			return (output, nil)
 		} else {
-			return fmt.Errorf("%w: Expected 1 or 3 arguments, 1 argument of \"-p\" to print alias list, and 3 arguments for an alias entry.", ErrInvalidArgCountAlias)
+			return "", fmt.Errorf("%w: Expected 1 or 3 arguments, 1 argument of \"-p\" to print alias list, and 3 arguments for an alias entry.", ErrInvalidArgCountAlias)
 		}
 	case 2:
-		return fmt.Errorf("%w: missing arguments, alias needs to be used in the following syntax: alias update = \"sudo apt-get update\" ensure the \"\" quotes are used to define the command", ErrInvalidArgCountAlias)
+		return "", fmt.Errorf("%w: missing arguments, alias needs to be used in the following syntax: alias update = \"sudo apt-get update\" ensure the \"\" quotes are used to define the command", ErrInvalidArgCountAlias)
 	case 3:
 		aliasSlic = append(aliasSlic, (ComAlias{name: args[0], value: args[2]}))
-		fmt.Println("new alias is: ", args[0], " = ", args[2])
-		return (nil)
+		// fmt.Println("new alias is: ", args[0], " = ", args[2])
+		return (string("new alias is: ", args[0], " = ", args[2]), nil)
 	default:
-		return fmt.Errorf("%w: Expected 1 or 3 arguments, 1 argument of \"-p\" to print alias list, and 3 arguments for an alias entry.", ErrInvalidArgCountAlias)
+		return "", fmt.Errorf("%w: Expected 1 or 3 arguments, 1 argument of \"-p\" to print alias list, and 3 arguments for an alias entry.", ErrInvalidArgCountAlias)
 	}
 }
