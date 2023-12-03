2f339069a2e70d95beec1edb89ab38971c10ff11
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 20:04:07 -0600

    Update alias.go

--- builtins/alias.go
+++ builtins/alias.go
@@ -46,7 +46,9 @@ func CommandAlias(w io.Writer, args ...string) (string, error) {
 	case 3:
 		aliasSlic = append(aliasSlic, (ComAlias{name: args[0], value: args[2]}))
 		// fmt.Println("new alias is: ", args[0], " = ", args[2])
-		output += "new alias is: ", string(args[0]), " = ", string(args[2])
+		arg1 := args[0]
+		arg2 := args[2]
+		output += "new alias is: ", arg1, " = ", arg2
 		return output, nil
 	default:
 		return "", fmt.Errorf("%w: Expected 1 or 3 arguments, 1 argument of \"-p\" to print alias list, and 3 arguments for an alias entry.", ErrInvalidArgCountAlias)
