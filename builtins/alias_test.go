3470f8e612de2b58f147696c2f6f17bc93a74352
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 20:21:44 -0600

    Update alias_test.go

--- builtins/alias_test.go
+++ builtins/alias_test.go
@@ -7,12 +7,18 @@ import (
 
 var str string
 
+func stringUser(str string) {
+	str += str
+	return
+}
+
 func TestCommandAliasPrintAliases(t *testing.T) {
 	var buf bytes.Buffer
 	str, err := CommandAlias(&buf, "-p")
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
+	stringUser(str)
 	expectedOutput := ""
 	if got := buf.String(); got != expectedOutput {
 		t.Errorf("Expected output %q, got %q", expectedOutput, got)
@@ -25,6 +31,7 @@ func TestCommandAliasAddAlias(t *testing.T) {
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
+	stringUser(str)
 	expectedOutput := "Alias name: newAlias, Alias value: echo Hello, World!"
 	if got := buf.String(); got != expectedOutput {
 		t.Errorf("Expected output %q, got %q", expectedOutput, got)
@@ -37,4 +44,5 @@ func TestCommandAliasInvalidArgs(t *testing.T) {
 	if err != ErrInvalidArgCountAlias {
 		t.Errorf("Expected error %v, got %v", ErrInvalidArgCountAlias, err)
 	}
+	stringUser(str)
 }
