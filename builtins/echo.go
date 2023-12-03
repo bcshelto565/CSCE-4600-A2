51a4266ecc1b814a0206de4e1268d4ebaad524ce
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-02 19:28:31 -0600

    Update echo.go

--- builtins/echo.go
+++ builtins/echo.go
@@ -10,7 +10,7 @@ var (
 	ErrInvalidArgCountEcho = errors.New("invalid argument count")
 )
 
-func Echo(w io.Writer, args ...string) (string, error) {
+func Echo(w io.Writer, args ...string) (error) {
 	switch len(args) {		// switch case for determining how to run the command
 	case 0:
 		return fmt.Errorf("%w: expected at least one argument, echo is used to echo a string, need a string to echo", ErrInvalidArgCountEcho)
