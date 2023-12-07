5d403aea76ac82c3b317482d5a836d5b7ecf31b7
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-07 13:18:28 -0600

    Update cat.go

--- builtins/cat.go
+++ builtins/cat.go
@@ -21,7 +21,7 @@ func PrintFileContents(w io.Writer, args ...string) error {
 		if err != (nil) {
 			return fmt.Errorf("%w: File is empty and cannot be read", EmptyFileError)
 		}
-		file_stat, staterr := os.Stat(args[0]).st_size
+		file_stat, staterr := os.Stat(args[0])
 		if file_stat.Size == 0 {
 			return fmt.Errorf("%w: File is empty and cannot be read", EmptyFileError)
 		}
