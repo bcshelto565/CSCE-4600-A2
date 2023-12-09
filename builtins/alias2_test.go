84913e35859352ba9f89a4f7890e9a08174387cf
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-09 17:41:09 -0600

    Update alias2_test.go

--- builtins/alias2_test.go
+++ builtins/alias2_test.go
@@ -8,12 +8,6 @@ import (
 	"fmt"
 )
 
-
-var (
-	/*ErrInvalidArgCountCat = errors.New("invalid argument count")
-	EmptyFileError = errors.New("invalid file")*/
-)
-
 func TestCommandAlias(t *testing.T) {
   var out bytes.Buffer
 	type args struct {
@@ -59,14 +53,6 @@ func TestCommandAlias(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// setup
-			/*if tt.unsetHomedir {
-				oldVal := builtins.HomeDir
-				t.Cleanup(func() {
-					builtins.HomeDir = oldVal
-				})
-				builtins.HomeDir = ""
-			}*/
-
 			// testing
 			if err := builtins.CommandAlias(&out, tt.args.args...); tt.wantErr != nil {
 				if !errors.Is(err, tt.wantErr) {
@@ -76,25 +62,6 @@ func TestCommandAlias(t *testing.T) {
 			} else if err != nil {
 				t.Fatalf("CommandAlias() unexpected error: %v", err)
 			}
-
-			// "happy" path
-      /*
-			file, err := os.Open(filNam)
-			if err != nil {
-				t.Fatalf("Could not open temp file")
-			}
-			file.Close()*/
-			/*d1, err := os.Stat(wd)
-			if err != nil {
-				t.Fatalf("Could not stat dir: %v", wd)
-			}
-			d2, err := os.Stat(tt.wantDir)
-			if err != nil {
-				t.Fatalf("Could not stat dir: %v", tt.wantDir)
-			}
-			if !os.SameFile(d1, d2) {
-				t.Errorf("Working Directory = %v, wantDir %v", wd, tt.wantDir)
-			}*/
 		})
 	}
 }
