60912c9a97540f3d921516ac683799153a011417
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-09 14:48:31 -0600

    Update cd.go

--- builtins/cd.go
+++ builtins/cd.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	ErrInvalidArgCountCd = errors.New("invalid argument count")
+	ErrInvalidArgCount = errors.New("invalid argument count")
 	HomeDir, _         = os.UserHomeDir()
 )
 
@@ -15,12 +15,12 @@ func ChangeDirectory(args ...string) error {
 	switch len(args) {
 	case 0: // change to home directory if available
 		if HomeDir == "" {
-			return fmt.Errorf("%w: no homedir found, expected one argument (directory)", ErrInvalidArgCountCd)
+			return fmt.Errorf("%w: no homedir found, expected one argument (directory)", ErrInvalidArgCount)
 		}
 		return os.Chdir(HomeDir)
 	case 1:
 		return os.Chdir(args[0])
 	default:
-		return fmt.Errorf("%w: expected zero or one arguments (directory)", ErrInvalidArgCountCd)
+		return fmt.Errorf("%w: expected zero or one arguments (directory)", ErrInvalidArgCount)
 	}
 }
