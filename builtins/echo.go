bfa0620304698a96d81e9a50656284b7e84d2c4b
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-09 16:00:34 -0600

    Update echo.go

--- builtins/echo.go
+++ builtins/echo.go
@@ -18,7 +18,8 @@ func Echo(w io.Writer, args ...string) (error) {
 		return fmt.Errorf("%w: expected at least one argument, echo is used to echo a string, need a string to echo", ErrInvalidArgCountEcho)
 	default:
 		ech := make([]string, 0)
-		ech = append(ech, args)
+		justargs := strings.Join(args,"")
+		ech = append(ech, justargs)
 		// PlaceholderErr = errors.New(ech)
 		_, err := fmt.Fprintln(w, strings.Join(ech, "\n"))
 		return err
