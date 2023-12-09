18ae5a920b837740e1ab58b1239cd9ca53417f1f
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-09 15:46:43 -0600

    Create echo2_test.go

---
+++ builtins/echo2_test.go
@@ -0,0 +1,93 @@
+package builtins_test
+
+import (
+	"bytes"
+	"errors"
+	"github.com/bcshelto565/CSCE-4600-A2/builtins"
+	"testing"
+	"fmt"
+)
+
+
+var (
+	/*ErrInvalidArgCountCat = errors.New("invalid argument count")
+	EmptyFileError = errors.New("invalid file")*/
+)
+
+func TestCommandAlias(t *testing.T) {
+  var out bytes.Buffer
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name         string
+		args         args
+    listExist    bool
+		wantFile     string
+		wantErr      error
+		wantOut      string
+	}{
+		{
+			name: "error too many args",
+			args: args{
+				args: []string{"abc ", "def "},
+			},
+			wantErr: builtins.ErrInvalidArgCountEcho,
+		},
+		{
+			name: "no args should throw invalid args error",
+      			args: args{
+				args: []string{},
+			},
+			wantErr: builtins.ErrInvalidArgCountEcho,
+		},
+		{
+			name: "valid arguments should print text",
+			args: args{
+				args: []string{"\"HelloWorld\""},
+			},
+			wantOut: fmt.Sprintln("HelloWorld"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// setup
+			/*if tt.unsetHomedir {
+				oldVal := builtins.HomeDir
+				t.Cleanup(func() {
+					builtins.HomeDir = oldVal
+				})
+				builtins.HomeDir = ""
+			}*/
+
+			// testing
+			if err := builtins.Echo(&out, tt.args.args...); tt.wantErr != nil {
+				if !errors.Is(err, tt.wantErr) {
+					t.Fatalf("Echo() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			} else if err != nil {
+				t.Fatalf("Echo() unexpected error: %v", err)
+			}
+
+			// "happy" path
+      /*
+			file, err := os.Open(filNam)
+			if err != nil {
+				t.Fatalf("Could not open temp file")
+			}
+			file.Close()*/
+			/*d1, err := os.Stat(wd)
+			if err != nil {
+				t.Fatalf("Could not stat dir: %v", wd)
+			}
+			d2, err := os.Stat(tt.wantDir)
+			if err != nil {
+				t.Fatalf("Could not stat dir: %v", tt.wantDir)
+			}
+			if !os.SameFile(d1, d2) {
+				t.Errorf("Working Directory = %v, wantDir %v", wd, tt.wantDir)
+			}*/
+		})
+	}
+}
