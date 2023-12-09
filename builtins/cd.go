fc481d2734e4ecc67f1aa742c0f18977e4b7334a
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-12-09 15:04:21 -0600

    Update cd.go

--- builtins/cd.go
+++ builtins/cd.go
@@ -16,12 +16,12 @@ func ChangeDirectory(args ...string) error {
 	switch len(args) {
 	case 0: // change to home directory if available
 		if HomeDir == "" {
-			return fmt.Errorf("%w: no homedir found, expected one argument (directory)", ErrInvalidArgCount)
+			return fmt.Errorf("%w: no homedir found, expected one argument (directory)", builtins.ErrInvalidArgCount)
 		}
 		return os.Chdir(HomeDir)
 	case 1:
 		return os.Chdir(args[0])
 	default:
-		return fmt.Errorf("%w: expected zero or one arguments (directory)", ErrInvalidArgCount)
+		return fmt.Errorf("%w: expected zero or one arguments (directory)", builtins.ErrInvalidArgCount)
 	}
 }
